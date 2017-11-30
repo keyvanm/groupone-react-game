c8d19df5f1a32dc6cc04fe2fff4af412d145fd43
Keyvan Mosharraf <kayvanmsh@gmail.com>
2017-11-29 21:40:51 -0800

    Loader lol

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,13 +1,39 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Progress } from 'reactstrap';
 import multivrsImg from './multivrsImg.jpg';
 
-const Cover = () => {
-  return (
-    <a href="/pr">
-      <img src={multivrsImg} style={{width: "100%"}} />
-    </a>
-  );
+class Cover extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { progress: 0 };
+  }
+
+  componentWillMount () {
+    const loaderFunc = setInterval(() => {
+      console.log("timeout");
+      const progress = this.state.progress + 1;
+      if (progress === 50) {
+        clearInterval(loaderFunc);
+        const fastLoaderFunc = setInterval(() => {
+          console.log("timeout");
+          const progress = this.state.progress + 5;
+          if (progress >= 100) {
+            window.location.assign("/pr");
+            clearInterval(fastLoaderFunc);
+          }
+          this.setState({ progress });
+        }, 80);
+      }
+      this.setState({ progress });
+    }, 40);
+  }
+  render () {
+    return (
+      <div style={{ backgroundImage: `url(${multivrsImg})`, backgroundSize: "contain", width: "100%", height: "500px" }}>
+        <Progress value={this.state.progress} />
+      </div>
+    );
+  }
 }
 
 export default Cover;
